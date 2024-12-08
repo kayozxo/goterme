d1f6311a69f889ba2e6dff6c39586e03976c1c1a
Manoj <penugurthimanoj@gmail.com>
2024-12-08 12:36:47 +0530

    added coins, coin collecting system, level system, coin collision

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"log"
-
+  "fmt"
+  "math/rand"
 	"github.com/gdamore/tcell/v2"
 )
+
+func drawString(screen tcell.Screen, x, y int, msg string) {
+  for index, char := range msg {
+    screen.SetContent(x + index, y, char, nil, tcell.StyleDefault)
+  }
+}
+
+func setupCoins(level int) []*Sprite {
+  coins := make([]*Sprite, level+2)
+  for index := range level+2 {
+    coins[index] = NewSprite('O', rand.Intn(20), rand.Intn(20))
+  }
+  return coins
+}
 
 func main () {
 	screen, err := tcell.NewScreen()
@@ -19,7 +34,13 @@ func main () {
 	}
 
 	//game init section
+
 	player := NewSprite('@', 10, 10)
+
+  coins := setupCoins(1)
+
+  score := 0
+  level := 1
 
 	// game loop
 	running := true
@@ -30,8 +51,19 @@ func main () {
 
 		player.Draw(screen)
 
+    for _, coin := range coins {
+      coin.Draw(screen)
+    }
+   
+    // ui
+
+    drawString(screen, 1, 1, fmt.Sprintf("Score: %d", score))
+    drawString(screen, 1, 2, fmt.Sprintf("Level: %d", level))
+
 		screen.Show()
 		//update logic
+
+    playerMoved := false
 
 		// getting the event
 		ev := screen.PollEvent()
@@ -44,13 +76,42 @@ func main () {
 						running = false
 					case 'w':
 						player.Y -= 1
+            playerMoved = true
 					case 'a':
 						player.X -= 1
+            playerMoved = true
 					case 's':
 						player.Y += 1
+            playerMoved = true
 					case 'd':
 						player.X += 1
+            playerMoved = true
 				}
 		}
+
+    // check for coin collisions
+    if playerMoved {
+      coinCollectedIndex := -1
+      for index, coin := range coins {
+        if coin.X == player.X && coin.Y == player.Y {
+          // collect coin
+          coinCollectedIndex = index
+          score++
+        }
+      }
+      // handle coin collisions
+      if coinCollectedIndex > -1 {
+        // swap target with last
+        coins[coinCollectedIndex] = coins[len(coins)-1]
+        // trim off last item
+        coins = coins[0:len(coins)-1]
+
+        if len(coins) == 0{
+          level++
+          coins = setupCoins(level)
+        }
+      }
+    } 
+
 	}
 }
